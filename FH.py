e745fae3f52c21f1577f2b57cfe5e6cd84a3955d
Gizmo900 <jahromia75@gmail.com>
2024-10-01 19:58:11 +0300

    Create FH.py

---
+++ FH.py
@@ -0,0 +1,35 @@
+
+with open("short_story.txt","w") as file:
+          file.write("gedagedigodagodao")
+
+
+
+
+with open("short_story.txt","r") as file:
+          print(file.read())
+
+
+with open("short_story.txt","a") as file:
+          file.write(" I am 10 years old")
+
+
+with open("short_story.txt","r") as file:
+          print(file.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
